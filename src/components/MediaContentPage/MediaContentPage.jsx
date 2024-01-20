0b11a3e6e54d730f70c6870c5847d18d2efe0caa
Luther Mendez <luther0211@gmail.com>
2024-01-20 15:46:03 -0600

    added the overview text to MediaContentType Component

--- src/components/MediaContentPage/MediaContentPage.jsx
+++ src/components/MediaContentPage/MediaContentPage.jsx
@@ -22,29 +22,38 @@ export default function MediaContentPage({ mediaType, mediaId }) {
     if (Object.keys(mediaContent).length <= 0) return <></>;
 
     return (
-        <div className="relative mt-4 ">
-            <img
-                src={`https://image.tmdb.org/t/p/original${mediaContent.backdrop_path}`}
-                alt={mediaContent.title}
-                className="blur-sm"
-            />
+        <>
+            <div className="relative mt-4">
+                <img
+                    src={`https://image.tmdb.org/t/p/original${mediaContent.backdrop_path}`}
+                    alt={mediaContent.title}
+                    className="blur-sm"
+                />
 
-            <div className="absolute top-1/2 px-4 -translate-y-1/2 w-full flex ">
-                <div className=" w-full flex justify-between">
-                    <img
-                        src={`https://image.tmdb.org/t/p/original${mediaContent.poster_path}`}
-                        alt={`${mediaContent.title} Poster`}
-                        className="h-40 shadow-xl"
-                    />
-                    <div className="pl-4 w-full overflow-hidden">
-                        <h1 className="text-lg font-bold mb-2">
-                            {mediaType == "movie"
-                                ? mediaContent.title
-                                : mediaContent.original_name}
-                        </h1>
+                <div className="absolute inset-0 px-2 py-2">
+                    <div className=" w-full flex justify-between">
+                        <img
+                            src={`https://image.tmdb.org/t/p/original${mediaContent.poster_path}`}
+                            alt={`${mediaContent.title} Poster`}
+                            className="h-40 shadow-xl"
+                        />
+                        <div className="pl-4 w-full">
+                            <h1 className="text-lg font-bold">
+                                {mediaType == "movie"
+                                    ? mediaContent.title
+                                    : mediaContent.original_name}
+                            </h1>
+                            <p className="text-xs mb-4">
+                                {mediaContent.release_date.split("-")[0]}
+                            </p>
+
+                            <p className="text-xs h-28 overflow-scroll">
+                                {mediaContent.overview}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
