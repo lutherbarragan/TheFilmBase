3fd22758dbd12e3f1340b194e8f5cdc240b8a0df
Luther Mendez <luther0211@gmail.com>
2024-01-24 22:11:28 -0600

    The detailed view now displays each of the genres under the poster image

--- src/components/MediaContentPage/MediaContentPage.jsx
+++ src/components/MediaContentPage/MediaContentPage.jsx
@@ -30,51 +30,60 @@ export default function MediaContentPage({ mediaType, mediaId }) {
     if (Object.keys(mediaContent).length <= 0) return <></>;
 
     return (
-        <>
-            <div className="relative mt-4">
-                <img
-                    src={`https://image.tmdb.org/t/p/original${mediaContent.backdrop_path}`}
-                    alt={mediaContent.title}
-                    className={`blur-sm object-cover transition-all duration-500 ease-in-out brightness-50 w-full ${
-                        expanded ? " h-80" : "h-56"
-                    }`}
-                />
+        <div className="relative mt-4">
+            <img
+                src={`https://image.tmdb.org/t/p/original${mediaContent.backdrop_path}`}
+                alt={mediaContent.title}
+                className={`blur-sm object-cover transition-all duration-500 ease-in-out brightness-50 w-full ${
+                    expanded ? " h-80" : "h-56"
+                }`}
+            />
 
-                <div className="absolute inset-0 p-2 pb-8 w-full flex justify-between">
+            <div className="absolute inset-0 p-2 pb-8 w-full flex justify-between">
+                <div className="w-2/6">
                     <img
                         src={`https://image.tmdb.org/t/p/original${mediaContent.poster_path}`}
                         alt={`${mediaContent.title} Poster`}
-                        className="h-40 shadow-xl"
+                        className=" w-full shadow-xl"
                     />
 
-                    <div className="pl-4 w-full overflow-hidden">
-                        <h1 className="text-lg font-bold">
-                            {mediaType == "movie"
-                                ? mediaContent.title
-                                : mediaContent.name ||
-                                  mediaContent.original_name}
-                        </h1>
-
-                        <p className="text-xs mb-1">
-                            {mediaContent?.release_date?.split("-")[0]}
-                        </p>
-
-                        <p className="text-xs my-2 font-semibold">
-                            {overviewText}
-                        </p>
+                    <div className="flex flex-wrap justify-center">
+                        {mediaContent.genres.map((genre, i) => (
+                            <span
+                                key={genre.name}
+                                className="leading-4 text-xs mr-1"
+                            >
+                                • {genre.name}
+                            </span>
+                        ))}
                     </div>
                 </div>
-                {overviewText.length > overviewLength ? (
-                    <button
-                        className="absolute bottom-3 left-1/2 text-red-600 font-semibold text-sm focus:outline-none"
-                        onClick={toggleExpansion}
-                    >
-                        {expanded ? "↑ Read Less ↑" : "↓ Read more ↓"}
-                    </button>
-                ) : (
-                    ""
-                )}
+
+                <div className="pl-2 w-4/6 overflow-hidden">
+                    <h1 className="text-lg font-bold">
+                        {mediaType == "movie"
+                            ? mediaContent.title
+                            : mediaContent.name || mediaContent.original_name}
+                    </h1>
+
+                    <p className="text-xs mb-1">
+                        {mediaContent.release_date?.split("-")[0]}
+                    </p>
+
+                    <p className="text-xs my-2 ">{overviewText}</p>
+                </div>
             </div>
-        </>
+
+            {overviewText.length > overviewLength ? (
+                <button
+                    className="absolute bottom-3 left-1/2 text-red-600 font-semibold text-sm focus:outline-none"
+                    onClick={toggleExpansion}
+                >
+                    {expanded ? "↑ Read Less ↑" : "↓ Read more ↓"}
+                </button>
+            ) : (
+                ""
+            )}
+        </div>
     );
 }
